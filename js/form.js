bc01778d27a81e02f0bc5217aee268cb942825fe
Galina <galinasale@gmail.com>
2016-10-17 10:16:52 +0000

    Commit

--- js/form.js
+++ js/form.js
@@ -15,23 +15,7 @@ $(function() {
         }).done(function() {
             $('form').html('<h4><center>Thank you for reaching out! </br> Your message has been successfully sent.</center></h4>')
         }).fail(function(xhr, err) {
-           // $('form').html(xhr.statusText);
-           $('#send').hide();
-        $('.warrning').show(function() {
-            $('#firstname, #secondname, #email, #message').on('focus', function() {
-                $('.warrning').hide();
-                $('#send').show();
-            });
+            $('form').html(xhr.statusText);
         });
-        $('form,').trigger("reset");
-    });
+    })
 });
-});
-/*function empty() {
-    var x;
-    x = document.getElementById("firstname").value;
-    if (x == "") {
-        alert("Enter a field");
-        return false;
-    };
-}*/
